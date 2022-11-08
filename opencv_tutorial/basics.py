ce7db0139422140ffeee688ae20b4e504d4e587b
gkye24 <geenak1020601@gmail.com>
2022-11-08 14:17:08 -0500

    :D

---
+++ opencv_tutorial/basics.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+
+img = cv.imread('photos/cat.jpg')
+cv.imshow('cat', img)
+
+# Converting image to grayscale
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+# Blur
+blur = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
+cv.imshow('Blur', blur)
+
+# Edge Cascade
+canny = cv.Canny(img, 125, 175)
+cv.imshow('Canny Edges', canny)
+
+# Dilating the image (made the outline thicker)
+dilated = cv.dilate(canny, (3,3), iterations=1)
+cv.imshow('Dilated', dilated)
+
+# Eroding (makes the outlines connect more)
+eroded = cv.erode(dilated, (3,3), iterations=1)
+cv.imshow('Eroded', eroded)
+
+# Resize
+# resized = cv.resize(img, (500,500))
+resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+# Cropping
+cropped = img[50:200, 200:400]
+cv.imshow('Cropped', cropped)
+
+cv.waitKey(0)

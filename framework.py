903dad93d5daba0edfd38145ab641a82e2b88905
Aparna <apapil23@bergen.org>
2022-11-17 00:13:04 -0500

    11/17

--- framework.py
+++ framework.py
@@ -6,33 +6,44 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
+count = 0
 class Window(QMainWindow):
-
+    
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Main")
         self.generalLayout = QGridLayout()
+        
 
         centralWidget = QWidget(self)
         centralWidget.setLayout(self.generalLayout)
         self.setCentralWidget(centralWidget)
 
         self._createPhoto()
-        self._createButtonsAndLabels()
+        self._createButtonsAndLabels(count)
 
     def _createPhoto(self):
         self.photo = Template()
         self.generalLayout.addWidget(self.photo, 0, 0)
+        
+    def _createMarker(self):
+        self.moveObject = MovingObject(50, 50, 40)
+        self.generalLayout.addWidget(self.moveObject)
 
-    def _createButtonsAndLabels(self):
+    def clickme(self):
+        print("pressed")
+
+    def _createButtonsAndLabels(self, count):
         self.countButton = QPushButton("Count")
         self.countButton.setFixedSize(100, 30)
         self.countLabel = QLabel("Circle Count:")
-        self.countDisplay = QLineEdit("(Number)")
+        self.countDisplay = QLineEdit("{0}".format(count))
         self.countDisplay.setFixedSize(400, 30)
         self.addMarkerButton = QPushButton("Add marker")
         self.removeMarkerButton = QPushButton("Remove marker")
 
+        self.addMarkerButton.clicked.connect(self.clickme)
+        
         self.smallGridLayout = QGridLayout()
         self.smallGridLayout.addWidget(self.countButton, 0, 1)
         self.smallGridLayout.addWidget(self.countLabel, 1, 0)
@@ -43,6 +54,37 @@ class Window(QMainWindow):
         self.generalLayout.addLayout(self.smallGridLayout, 0, 1)
 
 
+class MovingObject(QGraphicsEllipseItem):
+    def __init__(self, x, y, r):
+        super().__init__(0, 0, r, r)
+        self.setPos(x, y)
+        self.setBrush(Qt.blue)
+        self.setAcceptHoverEvents(True)
+
+    # mouse hover event
+    def hoverEnterEvent(self, event):
+        app.instance().setOverrideCursor(Qt.OpenHandCursor)
+
+    def hoverLeaveEvent(self, event):
+        app.instance().restoreOverrideCursor()
+
+    # mouse click event
+    def mousePressEvent(self, event):
+        pass
+
+    def mouseMoveEvent(self, event):
+        orig_cursor_position = event.lastScenePos()
+        updated_cursor_position = event.scenePos()
+
+        orig_position = self.scenePos()
+
+        updated_cursor_x = updated_cursor_position.x() - orig_cursor_position.x() + orig_position.x()
+        updated_cursor_y = updated_cursor_position.y() - orig_cursor_position.y() + orig_position.y()
+        self.setPos(QPointF(updated_cursor_x, updated_cursor_y))
+
+    def mouseReleaseEvent(self, event):
+        print('x: {0}, y: {1}'.format(self.pos().x(), self.pos().y()))
+        
 class PhotoLabel(QLabel):
 
     def __init__(self, *args, **kwargs):
